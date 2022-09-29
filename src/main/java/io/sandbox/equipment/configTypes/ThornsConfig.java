2a9752a2c088df73acd0d94dc2d24e08c3aafd76
Tyler Crocker <crocker.tylerj@gmail.com>
2022-09-28 22:27:23 -0700

    Altering the way defaults are handled

--- src/main/java/io/sandbox/equipment/configTypes/ThornsConfig.java
+++ src/main/java/io/sandbox/equipment/configTypes/ThornsConfig.java
@@ -1,13 +1,7 @@
 package io.sandbox.equipment.configTypes;
 
 public class ThornsConfig {
-  public boolean enabled;
-  public int maxLevel;
-  public float damagePerLevel;
-
-  public ThornsConfig() {
-    enabled = true;
-    maxLevel = 5;
-    damagePerLevel = 1.0F;
-  }
+  public boolean enabled = true;
+  public int maxLevel = 5;
+  public float damagePerLevel = 1.0F; // Do not include the F in the config file
 }
